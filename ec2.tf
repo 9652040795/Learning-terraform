966a36b8c657a4e16039b956b21e75714794642a
Asim Arain <chasim1982@gmail.com>
2018-11-24 12:32:17 +0500

    added ec2-security groups

--- ec2.tf
+++ ec2.tf
@@ -1,13 +1,18 @@
 #Creating an EC2 instance in Public Subnet must mention the "Subnet ID"#
 resource "aws_instance" "cloudelligent" {
-  ami = "ami-0ac019f4fcb7cb7e6"
+  ami = "ami-8e8f2fe1"
 #   ami = "${var.ami-id}"
   instance_type = "t2.micro"
 #  subnet_id = "subnet-0b03fbea26eafeb61"
 #  subnet_id = "${var.ec2-subnet}"
-  count = "3"
+  count = "2"
+
+  #EXISTING KEY PAIR OR CREATE ssh-keygen -f demo #it will give private & public keys, import public in aws
+  key_name = "power"
+  user_data = "${file("httpd.sh")}"
   subnet_id = "${element(var.ec2-subnets-id,count.index)}"
+  vpc_security_group_ids = ["${aws_security_group.ec2-sg.id}"]
   tags {
-    Name= "cloudelligent-ec2-${count.index+1}"
+    Name= "open-swan-vpn-${count.index+1}"
   }
 }
